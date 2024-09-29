44ad0b11d4fec5d4ee17d836fab6124ce15c39cf
Vishal Singh <vishal@curiticshealth.com>
2024-09-29 15:07:56 +0530

    Models and Entities are finished

--- src/main/java/com/scm/entities/User.java
+++ src/main/java/com/scm/entities/User.java
@@ -1,10 +1,16 @@
 package com.scm.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.aspectj.apache.bcel.classfile.Module.Provide;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,11 +33,11 @@ public class User {
     @Column(name = "user_name", nullable = false)
     private String name;
     @Column(unique = true, nullable = false)
-    private String email;
+    private String email; 
     private String password;
-    @Column(length = 10000)
+    @Column(length = 1000)
     private String about;
-    @Column(length = 10000)
+    @Column(length = 1000)
     private String profilePic;
     private String phoneNumber;
 
@@ -45,4 +51,10 @@ public class User {
 
     private Providers provider = Providers.SELF;
     private String providerUserInd;
+
+    // Add more fields if needed
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Contact> contacts = new ArrayList<>();
+
 }
